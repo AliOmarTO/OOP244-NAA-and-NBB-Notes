a2fd34295955a7a67f88a69942409083439e6ac0
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2022-10-11 13:09:07 -0400

    better output

--- NBB/10-Oct11/prg.cpp
+++ NBB/10-Oct11/prg.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 class output {
 public:
-   void print(int val) {
+   output& print(int val) {
       printf("%d", val);
+      return *this;
    }
-   void print(double val) {
+   output& print(double val) {
       printf("%.2lf", val);
+      return *this;
    }
-   void print(const char* val) {
+   output& print(const char* val) {
       printf(val);
+      return *this;
    }
 };
 int main() {
    output fout;
    int age = 35;
    double salary = 123456.45;
-   fout.print("Hello I am a progammer, I am ");
-   fout.print(age);
-   fout.print(" year old and I am make ");
-   fout.print(salary);
-   fout.print(" Dollara a year!\n");
+   fout.print("Hello I am a progammer, I am ").print(age).print(" year old and I am make ").print(salary).print(" Dollara a year!\n");
    return 0;
 }
