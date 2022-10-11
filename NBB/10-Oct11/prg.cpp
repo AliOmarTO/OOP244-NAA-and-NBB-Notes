a9ae1bf386070016eaaa8258b9fbc0961a074837
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2022-10-11 13:11:02 -0400

    How cout works

--- NBB/10-Oct11/prg.cpp
+++ NBB/10-Oct11/prg.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 class output {
 public:
-   output& print(int val) {
+   output& operator<<(int val) {
       printf("%d", val);
       return *this;
    }
-   output& print(double val) {
+   output& operator<<(double val) {
       printf("%.2lf", val);
       return *this;
    }
-   output& print(const char* val) {
+   output& operator<<(const char* val) {
       printf(val);
       return *this;
    }
@@ -19,6 +19,6 @@ int main() {
    output fout;
    int age = 35;
    double salary = 123456.45;
-   fout.print("Hello I am a progammer, I am ").print(age).print(" year old and I am make ").print(salary).print(" Dollara a year!\n");
+   fout << "Hello I am a progammer, I am " << age << " year old and I am make " << salary << " Dollara a year!\n";
    return 0;
 }
